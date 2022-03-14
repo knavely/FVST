fe6ead2994abbd71239fb3ce8ba9019b7d1fcb57
knavely <knavely@gmail.com>
2022-03-13 22:20:11 -0700

    Update T_Seven.py

--- T_Seven.py
+++ T_Seven.py
@@ -72,5 +72,5 @@ lightT7s = [t for t in ti if not is_heavy(t, 7)]
 snark = [(0, 2, 5),(0, 3, 5),(0, 4, 5), (1, 2, 4),(1, 2, 5),(1, 3, 4),(1, 3, 5),(2, 3, 6),(2, 4, 6),(2, 5, 6)]
 
 #there is exactly one light T_7
-lightT7 == [snark]
+lightT7s == [snark]
 
